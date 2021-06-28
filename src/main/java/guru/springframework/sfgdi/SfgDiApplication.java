fa155e873683656bc390ea40f50230022b56ed6c
Mahammad Yusifov <52427732+Megative19@users.noreply.github.com>
2021-06-28 17:51:44 +0400

    my edit

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,18 +4,16 @@ import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-
-		MyController myController = (MyController) ctx.getBean("myController");
-
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SfgDiApplication.class, args);
+        MyController myController = (MyController) applicationContext.getBean("myController");
+        String s = myController.sayHello();
+        System.out.println(s);
+    }
 
 }
